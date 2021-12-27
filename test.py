6a50c5792932dc814b58a1c6e4354549a8fc2aa8
sagarmk <mankarisagar@gmail.com>
2021-12-27 00:15:17 -0600

    init

---
+++ test.py
@@ -0,0 +1,8 @@
+from extract import Extract
+
+if __name__ == "__main__":
+
+    emp = Extract()
+    emp.search_keys('employment agreement')
+    links_to_download = emp.extract_links()
+    print("List of urls downloaded :: ",links_to_download)
